7ded52f1cc73373928dace8fbd12d1a1df58418a
praveenlodhi99 <praveenlodhi.official@gmail.com>
2024-09-01 23:22:49 +0530

    Object setup complete

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,19 @@
+"use client"
+
+import Experience from '@/components/Experience'
+import { OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='h-[90vh] w-[90vw]'>
+      <Canvas className='h-full w-full '>
+        {/* <color attach="background" args={["#213547"]} /> */}
+        <ambientLight />
+        <Experience />
+      </Canvas>
+    </div>
   )
 }
 
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,34 @@
+import { MeshReflectorMaterial, PresentationControls, Stage } from '@react-three/drei'
 import React from 'react'
 
 const Experience = () => {
-  return (
-    <div>Experience</div>
-  )
+    return (
+        <PresentationControls speed={1.5} global polar={[-0.1, Math.PI / 4]} rotation={[Math.PI / 8, Math.PI / 4, 0]} >
+            <Stage environment="city" intensity={0.6} castShadow={false}>
+                <mesh>
+                    <boxGeometry args={[1,1,1]}/>
+                    <meshNormalMaterial />
+                </mesh>
+            </Stage>
+
+            <mesh rotation={[-Math.PI / 2, 0, 0]}  position-y={0} >
+                <planeGeometry args={[170, 170]} />
+                <MeshReflectorMaterial
+                    blur={[300, 100]}
+                    resolution={2048}
+                    mixBlur={1}
+                    mixStrength={40}
+                    roughness={1}
+                    depthScale={1.2}
+                    minDepthThreshold={0.4}
+                    maxDepthThreshold={1.4}
+                    color="#101010"
+                    metalness={0.5}
+                />
+            </mesh>
+
+        </PresentationControls>
+    )
 }
 
 export default Experience
